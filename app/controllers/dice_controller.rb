8cb156721b41b3eb600187cb52d6afc4d2ef7699
Maya Sheriff <113066754+MayaS1111@users.noreply.github.com>
2024-06-04 20:51:58 +0000

    route, controller and erb file made and started

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,14 @@
+class DiceController < ActionController::Base
+  def roll
+    @num_dice = params.fetch("number_of_dice").to_i
+    @sides = params.fetch("how_many_sides").to_i
+    @rolls = []
+
+    @num_dice.times do
+      die = rand(1..@sides)
+
+      @rolls.push(die)
+    end
+    render({template: "display/roll_flexible"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-
+  get("/dice/:number_of_dice/:how_many_sides",{controller : "dice" , action : "roll"})
 end
